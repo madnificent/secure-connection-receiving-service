92a5b1f228f2a72e4636c37639dc0db9ae43daf2
Aad Versteden <madnificent@gmail.com>
2020-08-30 13:50:40 +0200

    /secure now also streams the request to the identifier
    The request is manipulated slightly in order to ensure contents arrive
    correctly.

--- app.js
+++ app.js
@@ -2,25 +2,53 @@
 
 import { app, query, errorHandler } from 'mu';
 import fs from 'fs';
+import net from 'net';
 
 // DANGER: removing processors we do not want from the template here
 app._router.stack = removeStackPreProcessors( app._router.stack );
 
-app.post('/secure', function( req, res ) {
+app.post('/secure', async function( req, res ) {
   let body = '';
   req.on('data', (data) => body += data );
-  req.on('end', (data) => {
+  req.on('end', async (data) => {
+    const headers = forwardedHeadersAsString(req.headers);
+    const fileBody = `${headers}\r\n${body}`;
+
+    // write the data to a file
     const filePath = "/data/request.txt";
-    const headers = headersToString(req.rawHeaders);
-    body = `${headers}\n${body}`;
-    fs.appendFile( filePath, body,
-                   () => res.send("Wrote to disk"),
+    fs.appendFile( filePath, fileBody,
+                   () => null,
                    (err) => {
                      if( err ) {
                        console.log(err);
-                       res.send("Booboo");
                      }
                    } );
+
+    // send out a request
+    const fixedHeader = "GET /nothing HTTP/1.1\r\nHost: identifier\r\nAccept-Encoding: identity";
+    const rawHttpRequest = `${fixedHeader}\r\n${fileBody}`;
+
+    const client = new net.Socket();
+    client
+      .connect( 80, "identifier", () => {
+        // client.write( `${firstLine}\n${secondLine}\n${fileBody}` );
+      })
+      .on('connect', () => client.write(rawHttpRequest))
+    // Pipe the response
+      .on('data', (d) => {
+        // NOTE: not sure how supported this is, but we want to write
+        // the raw content and this seems to be the way to go about
+        // it.
+        res.connection.write(d.toString());
+      })
+      .on('close', () => {
+        res.end();
+        console.log("CLOSED CONNECTION");
+      })
+      .on('error', () => {
+        res.end();
+        console.log("AN ERROR OCCURRED");
+      });
   });
 } );
 
@@ -52,16 +80,16 @@ function removeStackPreProcessors( originalStack ){
   return stack;
 }
 
-function headersToString( headers ) {
-  let isLabel = true; // true for labels, false for their values
-  let body = "";
-  for( const value of headers ) {
-    if( isLabel )
-      body += `${value}: `;
-    else
-      body += `${value}\n`;
+function forwardedHeadersAsString( headers ) {
+  const ignoreList = ["host", "accept-encoding"];
 
-    isLabel = !isLabel;
+  let body = "";
+  for( const key in headers ) {
+    if( ignoreList.includes( key.toLowerCase() ) ) {
+      // skip
+    } else {
+      body += `${key}: ${headers[key]}\r\n`;
+    }
   }
   return body;
 }
