6dd9042c8c8275b1726984c2491637ffba215b40
Aad Versteden <madnificent@gmail.com>
2020-08-30 15:40:39 +0200

    Write response to a file
    By writing the response to a file, we are readying ourselves for the
    encryption of the full body.  This should help once we get GPG
    running.

--- app.js
+++ app.js
@@ -12,43 +12,57 @@ app.post('/secure', async function( req, res ) {
   req.on('data', (data) => body += data );
   req.on('end', async (data) => {
     const headers = forwardedHeadersAsString(req.headers);
-    const fileBody = `${headers}\r\n${body}`;
+    const incomingFileBody = `${headers}\r\n${body}`;
 
     // write the data to a file
-    const filePath = "/data/request.txt";
-    fs.appendFile( filePath, fileBody,
-                   () => null,
-                   (err) => {
-                     if( err ) {
-                       console.log(err);
-                     }
-                   } );
+    const incomingFilePath = "/data/incomingRequest.txt";
+    fs.writeFile( incomingFilePath, incomingFileBody,
+                  (err) => {
+                    if( err ) {
+                      console.log(err);
+                    }
+                  } );
 
     // send out a request
-    const fixedHeader = "GET /nothing HTTP/1.1\r\nHost: identifier\r\nAccept-Encoding: identity";
-    const rawHttpRequest = `${fixedHeader}\r\n${fileBody}`;
+    const outgoingFilePath = "/data/outgoingRequest.txt";
+    fs.readFile( outgoingFilePath, 'utf8', (err, outgoingFileBody) => {
+      if( err ) {
+        console.log(`An error occurred whilst reading the request: ${err}`);
+      } else {
+        const fixedHeader = "GET /nothing HTTP/1.1\r\nHost: identifier\r\nAccept-Encoding: identity\r\nConnection: close";
+        const rawHttpRequest = `${fixedHeader}\r\n${outgoingFileBody}`;
 
-    const client = new net.Socket();
-    client
-      .connect( 80, "identifier", () => {
-        // client.write( `${firstLine}\n${secondLine}\n${fileBody}` );
-      })
-      .on('connect', () => client.write(rawHttpRequest))
-    // Pipe the response
-      .on('data', (d) => {
-        // NOTE: not sure how supported this is, but we want to write
-        // the raw content and this seems to be the way to go about
-        // it.
-        res.connection.write(d.toString());
-      })
-      .on('close', () => {
-        res.end();
-        console.log("CLOSED CONNECTION");
-      })
-      .on('error', () => {
-        res.end();
-        console.log("AN ERROR OCCURRED");
-      });
+        console.log(rawHttpRequest);
+
+        const client = new net.Socket();
+        let receivedData = "";
+        client
+          .connect( 80, "identifier", () => {
+            // client.write( `${firstLine}\n${secondLine}\n${fileBody}` );
+          })
+          .on('connect', () => client.write(rawHttpRequest))
+        // Pipe the response
+          .on('data', (d) => {
+            // NOTE: not sure how supported this is, but we want to write
+            // the raw content and this seems to be the way to go about
+            // it.
+            receivedData += d.toString();
+            res.connection.write(d.toString());
+          })
+          .on('close', () => {
+            res.end();
+            console.log("CLOSED CONNECTION");
+            fs.writeFile( "/data/incomingResponse.txt", receivedData,
+                          (err) => err
+                          ? console.log(`error writing file: ${err}`)
+                          : console.log("Wrote file") );
+          })
+          .on('error', () => {
+            res.end();
+            console.log("AN ERROR OCCURRED");
+          });
+      }
+    });
   });
 } );
 
@@ -81,7 +95,7 @@ function removeStackPreProcessors( originalStack ){
 }
 
 function forwardedHeadersAsString( headers ) {
-  const ignoreList = ["host", "accept-encoding"];
+  const ignoreList = ["host", "accept-encoding", "connection"];
 
   let body = "";
   for( const key in headers ) {
