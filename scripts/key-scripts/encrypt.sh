7c0c0fe6223d1e9f6871bb40a587e7dda389566f
Aad Versteden <madnificent@gmail.com>
2020-08-31 15:02:18 +0200

    Add script for encrypting a known file
    There is now a script which allows us to encrypt a specific file.
    By allowing us to encrypt a file, we should be able to draft a request
    and send that out.

---
+++ scripts/key-scripts/encrypt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -n "Creating folders... "
+
+mkdir -p /app/config/keys
+mkdir -p /app/data/secureconnection/.gnupg
+
+echo " DONE"
+
+echo -n "Copying over contents... "
+cp -a /app/data/secureconnection/.gnupg /root/
+echo " DONE"
+
+echo "Encrypting file $1 ... "
+gpg --recipient "secureproducer@redpencil.io" --output /app/$2 --encrypt /app/$1
+echo "DONE!"
+echo ""
+echo "Find your file in $2"
